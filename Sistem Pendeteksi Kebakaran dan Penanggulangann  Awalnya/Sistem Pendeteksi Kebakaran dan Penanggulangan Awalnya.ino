a956dadb2d3a506461fa1de96addb2b27a17256f
yasir4bdulkhalim <136903836+yasir4bdulkhalim@users.noreply.github.com>
2024-06-25 10:23:08 +0700

    Add files via upload

---
+++ Sistem Pendeteksi Kebakaran dan Penanggulangann  Awalnya/Sistem Pendeteksi Kebakaran dan Penanggulangan Awalnya.ino
@@ -0,0 +1,44 @@
+int buzzer=7;
+int pintu=5;
+int sprink=6;
+int api=4;
+int asap=A0;
+#define batas 150
+
+void setup() {
+  // Mendefinisikan Fungsi dari Pin yang Ingin Digunakan
+  pinMode(buzzer, OUTPUT);
+  pinMode(pintu, OUTPUT);
+  pinMode(sprink, OUTPUT);
+  pinMode(api, INPUT);
+  pinMode(asap, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int NilaiApi=digitalRead(api);
+  int NilaiAsap=analogRead(asap);
+  Serial.print("Nilai Api:");
+  Serial.println(NilaiApi);
+  Serial.print("Nilai Asap:");
+  Serial.println(NilaiAsap);
+  digitalWrite(pintu, HIGH);
+
+  if (NilaiAsap > batas||NilaiApi==0){
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(sprink, HIGH);
+    if (digitalRead(pintu==HIGH)){
+      digitalWrite(pintu, LOW); 
+    }else{}
+    Serial.println("API TERDETEKSI!!!");
+  }else{
+    digitalWrite(buzzer,LOW);
+    digitalWrite(sprink,LOW);
+  }
+  Serial.print("Kondisi Pintu :");
+  Serial.println(digitalRead(pintu));
+  Serial.print("Kondisi Sprink:");
+  Serial.println(digitalRead(sprink));
+  delay(1000);
+}
